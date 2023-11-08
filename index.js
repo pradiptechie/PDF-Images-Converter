1495685e102df12a856c5517c188c720368f7d0e
pradiptechie <kumarpradip3956@gmail.com>
2023-11-08 19:38:42 +0545

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	modified:   views/main.hbs

--- index.js
+++ index.js
@@ -45,7 +45,72 @@ app.post('/imageToPdf', upload.array('images'), (req, res) => {
 
 
 
-// app.post('/pdfToImage', (req,res)=>{
-//     res.end("Listining to PdfToImage");
+app.post('/pdfToImage', upload.single('pdfFile'), (req, res) => {
+    // if (!req.file) {
+    //     return res.status(400).send('No PDF file uploaded.');
+    // }
+    res.end("Listining to PdfToImage");
+});
+
+const PDF2Pic = require('pdf2pic');
+const archiver = require('archiver');
+
+// app.post('/pdfToImage', upload.single('pdfFile'), async (req, res) => {
+//     if (!req.file) {
+//         return res.status(400).send('No PDF file uploaded.');
+//     }
+
+//     const pdfBuffer = req.file.buffer;
+
+//     // Create a temporary directory for image files
+//     const tempDir = 'temp';
+//     if (!fs.existsSync(tempDir)) {
+//         fs.mkdirSync(tempDir);
+//     }
+
+//     // Configure pdf2pic options
+//     const pdf2pic = new PDF2Pic({
+//         density: 300,           // output image quality
+//         savename: 'pdf',        // name for the output images
+//         savedir: tempDir,       // save directory
+//         format: "jpg",          // output image format
+//         size: "1000x1000"       // output size
+//     });
+
+//     // Convert PDF to images
+//     const imagePaths = await pdf2pic.convertBuffer(pdfBuffer, -1);
+
+//     if (!imagePaths || imagePaths.length === 0) {
+//         return res.status(500).send('PDF to Image conversion failed.');
+//     }
+
+//     // Create a zip archive
+//     const output = fs.createWriteStream('pdf_images.zip');
+//     const archive = archiver('zip', {
+//         zlib: { level: 9 } // Set compression level
+//     });
+
+//     // Pipe archive data to the response
+//     output.on('close', () => {
+//         res.download('pdf_images.zip', 'pdf_images.zip', (err) => {
+//             if (err) {
+//                 console.error(err);
+//                 res.status(500).send('Zip creation error');
+//             }
+//             // Clean up the temporary directory and zip file
+//             fs.rmdirSync(tempDir, { recursive: true });
+//             fs.unlinkSync('pdf_images.zip');
+//         });
+//     });
+
+//     archive.pipe(output);
+
+//     // Add image files to the zip archive
+//     imagePaths.forEach((imagePath, index) => {
+//         const imageName = `page_${index + 1}.jpg`; // Naming images serially
+//         archive.file(imagePath, { name: imageName });
+//     });
+
+//     archive.finalize();
 // });
 app.listen(port, () => console.log(`App listening on port ${port}!`));
